9a799a2dc2b03c74ad90689a6c925b628f0b17a3
huzaifamk <huzaifamk@pm.me>
2025-05-01 23:04:55 +0500

    Flow test without FP

--- frontend/src/components/Ask.vue
+++ frontend/src/components/Ask.vue
@@ -16,21 +16,23 @@
 <script setup lang="ts">
 
 import {ref} from 'vue'
+import axios from "axios";
 
 const message = ref("")
 const answer = ref("")
-const PROXY_ADDR = "http://localhost:6191/test-endpoint"
+const PROXY_ADDR = "http://localhost:6193/test-endpoint"
 
 async function ask() {
-  let res = await window.fetch(PROXY_ADDR, {
-    method: "POST",
-    headers: {
-      'Content-Type': "application/json",
-      'Accept': "application/json",
-    },
-    body: JSON.stringify({data: message.value})
-  })
-  answer.value = await res.text()
+  let res = await axios.post(PROXY_ADDR,
+      {data: message.value},
+      {
+        headers: {
+          'Content-Type': "application/json",
+          'Accept': "application/json",
+        }
+      }
+  )
+  answer.value = res.data.data
 }
 
 function clear() {
