7635ff6ff581704633cded0b6dc9230395948eca
huzaifamk <huzaifamk@pm.me>
2025-05-03 15:11:12 +0500

    Modified the reverse proxy using the old code from echo server, now creates a new request and forwards it

--- reverse-proxy/src/main.rs
+++ reverse-proxy/src/main.rs
@@ -1,47 +1,223 @@
 use async_trait::async_trait;
-use log::info;
-use pingora_core::prelude::*;
-use pingora_proxy::{ProxyHttp, Session};
-use simplelog::{ConfigBuilder, LevelFilter, WriteLogger};
-use std::fs;
+use bytes::Bytes;
+use clap::Parser;
+use serde::{Deserialize, Serialize};
+use std::net::ToSocketAddrs;
 
-struct ReverseProxy;
+use pingora::Result;
+use pingora::http::{ResponseHeader, StatusCode};
+use pingora::proxy::{ProxyHttp, Session};
+use pingora::server::Server;
+use pingora::server::configuration::Opt;
+use pingora::upstreams::peer::HttpPeer;
+
+use chrono::Local;
+use env_logger;
+use log::*;
+use reqwest::Client;
+use std::collections::HashMap;
+use std::fs::OpenOptions;
+use std::io::Write;
+
+const UPSTREAM_HOST: &str = "localhost";
+const UPSTREAM_IP: &str = "0.0.0.0";
+const BACKEND_PORT: u16 = 3000;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RequestBody {
+    data: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ResponseBody {
+    data: String,
+}
+
+pub struct EchoProxy {
+    addr: std::net::SocketAddr,
+}
+
+pub struct MyCtx {
+    _buffer: Vec<u8>,
+}
+
+impl EchoProxy {
+    async fn handle_request(session: &mut Session) -> Result<Option<ResponseBody>> {
+        let mut body = Vec::new();
+        loop {
+            match session.read_request_body().await? {
+                Some(chunk) => body.extend_from_slice(&chunk),
+                None => break,
+            }
+        }
+
+        match serde_json::de::from_slice::<RequestBody>(&body) {
+            Ok(request_body) => {
+                let request_url = session.req_header().uri.path().to_string();
+                debug!(
+                    "Creating a new request to http://localhost:{}{}",
+                    BACKEND_PORT, request_url
+                );
+                let client = Client::new();
+                let mut map = HashMap::new();
+                map.insert("data", request_body.data);
+                let res = client
+                    .post(format!("http://localhost:{}{}", BACKEND_PORT, request_url))
+                    .json(&map)
+                    .send()
+                    .await
+                    .unwrap();
+
+                debug!(
+                    "POST {}, Host: localhost:{}, response code: {}",
+                    BACKEND_PORT,
+                    res.status(),
+                    request_url
+                );
+
+                Ok(Some(res.json().await.unwrap()))
+            }
+            Err(err) => {
+                error!("ERROR: {err}");
+                Ok(None)
+            }
+        }
+    }
+
+    async fn set_headers(
+        response_status: StatusCode,
+        body_bytes: &Vec<u8>,
+        session: &mut Session,
+    ) -> Result<()> {
+        let mut header = ResponseHeader::build(response_status, None)?;
+        header
+            .append_header("Content-Length", body_bytes.len().to_string())
+            .unwrap();
+        header
+            .append_header("Access-Control-Allow-Origin", "*".to_string())
+            .unwrap();
+        header
+            .append_header("Access-Control-Allow-Methods", "POST".to_string())
+            .unwrap();
+        header
+            .append_header("Access-Control-Allow-Headers", "Content-Type".to_string())
+            .unwrap();
+        header
+            .append_header("Access-Control-Max-Age", "86400".to_string())
+            .unwrap();
+        session.write_response_header_ref(&header).await
+    }
+}
 
 #[async_trait]
-impl ProxyHttp for ReverseProxy {
-    type CTX = ();
-
-    fn new_ctx(&self) -> Self::CTX {}
+impl ProxyHttp for EchoProxy {
+    type CTX = MyCtx;
+    fn new_ctx(&self) -> Self::CTX {
+        MyCtx { _buffer: vec![] }
+    }
 
     async fn upstream_peer(
         &self,
         _session: &mut Session,
         _ctx: &mut Self::CTX,
-    ) -> Result<Box<pingora_core::upstreams::peer::HttpPeer>> {
-        Ok(Box::from(HttpPeer::new(
-            String::from("127.0.0.1:3000"),
-            false,
-            String::from(""),
-        )))
+    ) -> Result<Box<HttpPeer>> {
+        let peer: Box<HttpPeer> =
+            Box::new(HttpPeer::new(self.addr, false, UPSTREAM_HOST.to_owned()));
+        Ok(peer)
+    }
+
+    async fn request_filter(&self, session: &mut Session, _ctx: &mut Self::CTX) -> Result<bool>
+    where
+        Self::CTX: Send + Sync,
+    {
+        let mut response_body = ResponseBody {
+            data: String::from(""),
+        };
+        let mut response_status = StatusCode::OK;
+
+        let res = EchoProxy::handle_request(session).await;
+        match res {
+            Ok(Some(res)) => {
+                response_body = res;
+            }
+            Ok(None) => {
+                response_status = StatusCode::BAD_REQUEST;
+            }
+            Err(err) => {
+                error!("ERROR: {err}");
+                response_status = StatusCode::INTERNAL_SERVER_ERROR;
+            }
+        }
+
+        let response_body_bytes = serde_json::ser::to_vec(&response_body).unwrap();
+        EchoProxy::set_headers(response_status, &response_body_bytes, session).await?;
+        session
+            .write_response_body(Some(Bytes::from(response_body_bytes)), true)
+            .await?;
+
+        Ok(true)
+    }
+
+    async fn logging(
+        &self,
+        session: &mut Session,
+        _e: Option<&pingora::Error>,
+        ctx: &mut Self::CTX,
+    ) {
+        let response_code = session
+            .response_written()
+            .map_or(0, |resp| resp.status.as_u16());
+        // access log
+        info!(
+            "{} response code: {response_code}",
+            self.request_summary(session, ctx)
+        );
     }
 }
 
+// RUST_LOG=INFO cargo run proxy
 fn main() {
-    // Initialize logger
-    let log_file = fs::File::create("log.txt").expect("Failed to create log file");
-    let config = ConfigBuilder::new().set_time_to_local(true).build();
-    WriteLogger::init(LevelFilter::Debug, config, log_file).expect("Failed to initialize logger");
+    let file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("log.txt")
+        .expect("Can't create file!");
 
-    info!("Starting server...");
+    let target = Box::new(file);
 
-    let mut server = Server::new(None).unwrap();
-    server.bootstrap();
+    env_logger::Builder::new()
+        .target(env_logger::Target::Pipe(target))
+        .filter(None, LevelFilter::Debug)
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "[{} {} {}:{}] {}",
+                Local::now().format("%Y-%m-%d %H:%M:%S%.3f"),
+                record.level(),
+                record.file().unwrap_or("unknown"),
+                record.line().unwrap_or(0),
+                record.args()
+            )
+        })
+        .init();
 
-    let mut proxy = pingora_proxy::http_proxy_service(&server.configuration, ReverseProxy);
+    let opt = Opt::parse();
+    let mut my_server = Server::new(Some(opt)).unwrap();
+    my_server.bootstrap();
 
-    proxy.add_tcp("0.0.0.0:6193");
+    let mut my_proxy = pingora::proxy::http_proxy_service(
+        &my_server.configuration,
+        EchoProxy {
+            addr: (UPSTREAM_IP.to_owned(), BACKEND_PORT)
+                .to_socket_addrs()
+                .unwrap()
+                .next()
+                .unwrap(),
+        },
+    );
 
-    server.add_service(proxy);
+    my_proxy.add_tcp("127.0.0.1:6193");
 
-    server.run_forever();
+    my_server.add_service(my_proxy);
+    my_server.run_forever();
 }
