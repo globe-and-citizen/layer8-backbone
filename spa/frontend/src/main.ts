baca821390121b542c99c7f6abfa0e7dd1a5d48d
Osoro Bironga <fanosoro@gmail.com>
2025-07-13 12:51:00 +0300

    trial loader

--- spa/frontend/src/main.ts
+++ spa/frontend/src/main.ts
@@ -8,22 +8,13 @@ import { initEncryptedTunnel, ServiceProvider } from "interceptor-wasm"
 let forward_proxy_url = import.meta.env.VITE_FORWARD_PROXY_URL || 'http://localhost:6191';
 let backend_url = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
 
-const layer8_ = async () => {
-    try {
-        let providers = [ServiceProvider.new(backend_url)];
-        await initEncryptedTunnel(forward_proxy_url, providers).finally(() => {
-            console.log('Encrypted tunnel initialized successfully');
-        });
-    } catch (err) {
-        throw new Error(`Failed to initialize encrypted tunnel: ${err}`);
-    }
-};
-
-// we need the promise to resolve before mounting the app
-await layer8_();
+try {
+    let providers = [ServiceProvider.new(backend_url)];
+    initEncryptedTunnel(forward_proxy_url, providers);
+} catch (err) {
+    throw new Error(`Failed to initialize encrypted tunnel: ${err}`);
+}
 
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
